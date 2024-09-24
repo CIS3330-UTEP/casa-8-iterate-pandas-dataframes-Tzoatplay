221ce1d811f38707923dd766a49d41359a9d3a1e
Tzoatplay <aetrevizo2@miners.utep.edu>
2024-09-23 18:09:20 -0600

    CASA8

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,18 @@
+import pandas as pd
+df=pd.read_csv('./big-mac-full-index.csv')
+df.head()
 
+#index method
+
+# for index in df.index:
+#     print(df['date'][index])
+
+#iterrows method
+
+for i, r in df.iterrows():
+    print(r['name'],
+        r['iso_a3'])
+    
+#apply method
+
+print(df.apply(lambda row:row['date'], axis = 1))
